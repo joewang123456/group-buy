1c2d40f2d83debf9f0a9b03939e192e0173025e1
zkj <zkj@zkj.local>
2017-10-09 18:27:04 +0800

    group

--- js/index.js
+++ js/index.js
@@ -1,18 +1,61 @@
-function timeDown (milliSecond) {
-
-    function formate (val) {
-        if(val > 9) return val;
-        return `0${val}`;
+$(function(){
+    // alert(111);
+    let tool = {
+        timeFormate: function (val) {
+            if(val > 9) return val;
+            return `0${val}`;
+        },
+        timeDown: function (milliSecond) {
+            let s,m,h;
+            h = parseInt(milliSecond / 1000 / 60 / 60 );
+            m = parseInt(milliSecond / 1000 / 60 % 60 );
+            s = parseInt(milliSecond / 1000 % 60 );
+            return {
+                hour: this.timeFormate(h),
+                minute: this.timeFormate(m),
+                second: this.timeFormate(s)
+            }
+        }    
     }
-    
-    let s,m,h;
-    h = parseInt(milliSecond / 1000 / 60 / 60 );
-    m = parseInt(milliSecond / 1000 / 60 );
-    s = parseInt(milliSecond / 1000 % 60 );
 
-    return {
-        hour: formate(h),
-        minute: formate(m),
-        second: formate(s)
-    }
-}
+    // detail page
+    ;(function(){
+        function countTime(time){
+            let timeObj = tool.timeDown(time);
+            // 尽量减少DOM操作
+            if(timeObj.hour != $('.cont .count-down-item:nth-child(1)').text()){
+                $('.cont .count-down-item:nth-child(1)').text(timeObj.hour);
+            }                            
+            if(timeObj.minute != $('.cont .count-down-item:nth-child(2)').text()){
+                $('.cont .count-down-item:nth-child(2)').text(timeObj.minute);
+            }                            
+            if(timeObj.second != $('.cont .count-down-item:nth-child(3)').text()){
+                $('.cont .count-down-item:nth-child(3)').text(timeObj.second);
+            }                            
+        }
+        // $.ajax('',function(data){
+            let time = 86500000;
+            countTime(time);
+            let timer = setInterval(function(){
+                if(time>0){
+                    time -= 1000;
+                    countTime(time);                    
+                }else{
+                    clearInterval(timer);
+                }
+            },1000)
+        // })
+    })()
+
+    // launch page
+    ;(function(){
+        $('.launch footer').click(function(){
+            $('.launch .mask').fadeIn('400');
+        })
+        $('.launch .close').click(function(){
+            $('.launch .mask').fadeOut('100');
+        })
+    })()
+})
+
+
